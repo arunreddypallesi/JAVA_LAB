f9746801f66c1725b137a6aa597320a7124ae0b9
11249A267 <11249a267@kanchiuniv.ac.in>
2026-08-20 01:34:34 -0800

    Create if_statement.java

---
+++ if_statement.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class IfExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            System.out.println("The number is positive.");
+        }
+    }
+}
